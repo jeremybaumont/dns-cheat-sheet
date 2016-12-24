2231e49ae05bbbc8b6276a5c798a13120c341003
Lorin Hochstein <lhochstein@netflix.com>
2016-12-24 10:26:49 -0800

    Initial

--- dns.rb
+++ dns.rb
@@ -3,6 +3,22 @@ cheatsheet do
     docset_file_name 'dns'
     keyword 'dns'
     category do
-        id ''
+        id 'Client-side lookup'
+        entry do
+            name 'Dig (simple)'
+            notes <<-'END'
+            ```
+            dig +short www.example.com
+            ```
+            END
+        end
+        entry do
+            name 'Custom DNS server'
+            notes <<-'END'
+            ```
+            dig +short www.example.com @8.8.8.8
+            ```
+            END
+        end
     end
 end
